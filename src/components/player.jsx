3c1d4331c48d56f8daa9370336a78b9ec41d35b5
nathanbelaynehf <nathanbelayneh.1f@gmail.com>
2024-10-20 05:40:20 -0800

    Tic Toe React commit

---
+++ src/components/player.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+export default function Player({initialName,symbol,isActive,onchangeName}){
+    const [playerName,setPlayerName]= useState(initialName);
+    const [isEditing,setIsEditing]=useState(false);
+
+function handleEditClick(){
+  setIsEditing((editing)=>!editing);
+  if(isEditing){
+  onchangeName(symbol,playerName);
+}}
+
+  function handleChange(event){
+  
+    setPlayerName(event.target.value);
+  }
+
+ let editablePlayerName=<span className="player-name">{playerName}</span>;
+ let btnCaption='Edit';
+     if(isEditing){
+       editablePlayerName=<input type="text" required value={playerName} onChange={handleChange}/>
+       btnCaption="Save";
+       }
+    return (
+        <li className={isActive?'active':undefined}>
+        <span className="player">
+            {editablePlayerName}
+          <span className="player-symbol">{symbol}</span>
+        </span>
+        <button onClick={handleEditClick}>{btnCaption}</button>
+        </li>
+    );
+}
